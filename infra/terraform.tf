683653ac05d7597beb6044b04572a4b3a51bb9c8
shima8823 <taiyaki1275@gmail.com>
2026-01-22 10:58:36 +0900

    feat backend s3 for sharing tfstate

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -7,4 +7,12 @@ terraform {
   }
 
   required_version = "~> 1.14.0"
+
+  backend "s3" {
+    bucket       = "terraform-state-shima-3668f6c4"
+    key          = "state/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
