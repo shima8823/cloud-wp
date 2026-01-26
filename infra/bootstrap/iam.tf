673bcf0ef0d921411ecfb1f0f66b0845d7293e31
shima8823 <taiyaki1275@gmail.com>
2026-01-26 19:10:02 +0900

    feat: attach TerraformAdminRole to IAM user who is executing

--- infra/bootstrap/iam.tf
+++ infra/bootstrap/iam.tf
@@ -31,3 +31,20 @@ resource "aws_iam_role_policy_attachment" "admin_access" {
   role       = aws_iam_role.terraform_admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_user_policy" "allow_assume_terraform_admin_role" {
+  # 現在の実行者が IAM User の場合のみリソースを作成する
+  count = length(regexall(":user/", data.aws_caller_identity.current.arn)) > 0 ? 1 : 0
+  name  = "AllowAssumeTerraformAdminRole"
+  user  = split("/", data.aws_caller_identity.current.arn)[1]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = aws_iam_role.terraform_admin.arn
+      }
+    ]
+  })
+}
